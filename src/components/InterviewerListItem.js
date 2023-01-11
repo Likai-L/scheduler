f38f77e0d097f8899a3cea18d4e5d8a5ac11e85f
Likai Liu <likailiu2@gmail.com>
2023-01-11 21:24:08 +0800

    Create a InterviewerListItem component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import classNames from "classnames";
+
+import "./InterviewerListItem.scss";
+
+function InterviewerListItem(props) {
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+  return (
+    <li
+      className={interviewerClass}
+      onClick={() => {
+        props.setInterviewer(props.id);
+      }}
+    >
+      <img
+        className="interviewer__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
+
+export default InterviewerListItem;
