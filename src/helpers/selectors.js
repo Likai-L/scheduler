ebba38a77701f638edd15adbfc6364041e82c0bc
Likai Liu <likailiu2@gmail.com>
2023-01-13 07:38:39 +0800

    Add a selector for fetching interviewer data given their id

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -8,3 +8,17 @@ export function getAppointmentsForDay(state, day) {
   });
   return appointments;
 }
+
+export function getInterview(state, interview) {
+  if (!interview || !interview.interviewer) return null;
+  const interviewerId = interview.interviewer;
+  const interviewersArray = Object.values(state.interviewers);
+  const interviewerInfo = interviewersArray.filter(
+    (interviewer) => interviewer.id === interviewerId
+  )[0];
+  if (!interviewerInfo) return null;
+  return {
+    ...interview,
+    interviewer: interviewerInfo,
+  };
+}
