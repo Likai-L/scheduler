18829a4253a16b9e5a834127dcd4ff65c098f53a
Likai Liu <likailiu2@gmail.com>
2023-01-14 08:33:20 +0800

    add useVisualMode to help the conditional rendering

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -8,18 +8,36 @@ import Show from "./Show";
 
 import Empty from "./Empty";
 
+import Form from "./Form";
+
+import useVisualMode from "hooks/useVisualMode";
+
 function Appointment(props) {
+  const SHOW = "SHOW";
+  const EMPTY = "EMPTY";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? (
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
-      ) : (
-        <Empty />
       )}
+      {mode === EMPTY && (
+        <Empty
+          onAdd={() => {
+            transition(CREATE);
+          }}
+        />
+      )}
+      {mode === CREATE && <Form interviewers={[]} onCancel={back} />}
     </article>
   );
 }
